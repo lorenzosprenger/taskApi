4a736c4ed4f3f236c82a84645cf30523b7bee668
Arthur <aspossebonn@gmail.com>
2024-04-12 12:26:21 -0300

    atualizando

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const cors = require('cors');
+const router = require('./routes/taskRouter');
 const app = express();
 
-
 app.set('port', process.env.PORT || 3007);
+app.use(cors());
+app.use('/api', router);
+app.use(express.json());
 
 module.exports = app;
